8e013fd50fd4a57e27b99e752a3e3466d4cff365
Daiwik Gupta <130734114+Daiwik07@users.noreply.github.com>
2024-11-25 14:12:06 +0530

    Update IndianMedalWinningAthletes.jsx

--- src/components/IndianMedalWinningAthletes.jsx
+++ src/components/IndianMedalWinningAthletes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Images } from '../Images/Images';
+import { Images } from '../images/Images';
 import { Link,ScrollRestoration } from 'react-router-dom';
 
 const athletes = [
